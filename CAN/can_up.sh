47217b5f42b9485242ca07c8058d532b71f7def8
Jun-yong-lee <wnsdyd1569@naver.com>
2025-08-21 22:51:38 +0900

    Add | can1 setup

--- CAN/can_up.sh
+++ CAN/can_up.sh
@@ -1,5 +1,7 @@
 sudo modprobe can
 sudo modprobe kvaser_usb
 sudo ip link set can0 type can bitrate 500000
+sudo ip link set can1 type can bitrate 1000000
 sudo ifconfig can0 up
+sudo ifconfig can1 up
 # workon dmd
